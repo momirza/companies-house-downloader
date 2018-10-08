c7c196d6cd9392277db159f5c016cbf68f766d92
Mo Mirza <mo.mirza@iwoca.co.uk>
2018-10-08 22:53:25 +0100

    do not use env var for date

--- companies_house_to_s3.sh
+++ companies_house_to_s3.sh
@@ -4,16 +4,18 @@ set -e
 
 tmpdir=$(mktemp -d)
 
+date=$(date '+%Y-%m-%d')
+
 cd $tmpdir
 
-wget http://download.companieshouse.gov.uk/$URL_PATH-$DATE.zip
+wget http://download.companieshouse.gov.uk/$URL_PATH-$date.zip
 
-unzip $URL_PATH-$DATE.zip
+unzip $URL_PATH-$date.zip
 
-aws s3 cp $URL_PATH-$DATE.$EXTENSION s3://iwoca-coho-test//
+aws s3 cp $URL_PATH-$date.$EXTENSION s3://iwoca-coho-test/$DATA_SOURCE_TYPE/
 
 cd
 
 rm -r $tmpdir
 
-aws glue start-job-run --job-name $DATA_SOURCE_TYPE-csv-to-parquet --arguments "{\"--date\": \"$DATE\"}" --region eu-west-1
+aws glue start-job-run --job-name $DATA_SOURCE_TYPE-csv-to-parquet --arguments "{\"--date\": \"$date\"}" --region eu-west-1
